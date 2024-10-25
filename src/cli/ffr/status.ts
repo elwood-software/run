79fb6d45caba0be2aa70597758da4cc35ff51a5a
Travis Kuhl <travis.kuhl@gmail.com>
2024-10-25 10:11:11 -0700

    status and events

--- src/cli/ffr/status.ts
+++ src/cli/ffr/status.ts
@@ -1,5 +1,16 @@
 import type { FFrCliContext } from "../../types.ts";
 
+type Result = {
+  success: boolean;
+  status: string;
+  result: string;
+  summary: string;
+  short_summary: string;
+  reason: string;
+  started_at: string;
+  ended_at: string;
+};
+
 export default async function main(ctx: FFrCliContext) {
   const { args, storage } = ctx;
   const id = args._[1] ?? storage.lastId;
@@ -10,5 +21,15 @@ export default async function main(ctx: FFrCliContext) {
     Deno.exit(1);
   }
 
-  await Promise.resolve();
+  const r = await ctx.api<Result>(`/run/${id}`);
+
+  [
+    ["Status", r.status],
+    ["Result", r.result],
+    ["Started At", r.started_at],
+    ["Ended At", r.ended_at],
+    ["Summary", r.summary],
+    ["Short Summary", r.short_summary],
+    ["Reason", r.reason],
+  ].forEach(([name, value]) => console.log(`${name}: ${value ?? "-"}`));
 }
--- src/cli/ffr/watch.ts
+++ src/cli/ffr/watch.ts
@@ -1,5 +1,10 @@
 import type { FFrCliContext } from "../../types.ts";
 
+type Response = {
+  status: string;
+  events: Array<{ id: string; data: { text: string } }>;
+};
+
 export default async function main(ctx: FFrCliContext) {
   const { args, storage } = ctx;
   const id = args._[1] ?? storage.lastId;
@@ -10,11 +15,31 @@ export default async function main(ctx: FFrCliContext) {
     Deno.exit(1);
   }
 
-  const { events, status } = await ctx.api(`/run/${id}/events`);
+  let { events, status } = await ctx.api<Response>(`/run/${id}/events`);
 
   if (status === "queued") {
     console.log("Run is still in the queue. Check back in a few seconds");
     Deno.exit(0);
+  }
+
+  if (status === "running") {
+    while (status === "running") {
+      await new Promise((r) => setTimeout(r, 1000));
+      const r = await ctx.api<Response>(`/run/${id}/events`);
+      status = r.status;
+
+      for (const evt of r.events) {
+        if (!evt.data.text) {
+          continue;
+        }
+
+        if (events.find((e) => e.id === evt.id)) {
+          continue;
+        }
+
+        events.push(evt);
+      }
+    }
   }
 
   for (const evt of events) {
