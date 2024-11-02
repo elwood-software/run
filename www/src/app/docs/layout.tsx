0d679759931797501b266163eb98b2bf0f9e7ca6
Travis Kuhl <travis.kuhl@gmail.com>
2024-11-01 20:47:45 -0700

    www mobile docs

--- www/src/app/docs/layout.tsx
+++ www/src/app/docs/layout.tsx
@@ -1,6 +1,8 @@
 import {PropsWithChildren} from 'react';
 import type {Metadata} from 'next';
+import Link from 'next/link';
 
+import {FFrLogo} from '@/components/ffr-logo';
 import {DocsMenu} from './menu';
 
 export const metadata: Metadata = {
@@ -9,12 +11,17 @@ export const metadata: Metadata = {
 
 export default function Layout(props: PropsWithChildren) {
   return (
-    <div className="h-screen w-full grid grid-cols-[300px_5fr]">
-      <div className="">
-        <DocsMenu />
+    <div className="h-screen w-screen grid grid-rows-[60px_auto] md:grid-cols-[300px_5fr]">
+      <div className="border-b grid grid-cols-3 items-center justify-center">
+        <div className="ml-6">
+          <DocsMenu />
+        </div>
+        <div className="flex items-center justify-center font-bold font-mono">
+          Documentation
+        </div>
       </div>
-      <div className="grow">
-        <article className="prose dark:prose-invert lg:prose-base mx-auto max-w-6xl px-24 py-12">
+      <div className="grow overflow-y-auto md:overflow-y-visible">
+        <article className="prose dark:prose-invert lg:prose-base mx-auto w-screen md:max-w-6xl px-6 md:px-24 py-6 md:py-12">
           {props.children}
         </article>
       </div>
--- www/src/components/ffr-logo.tsx
+++ www/src/components/ffr-logo.tsx
@@ -1,6 +1,11 @@
-export function FFrLogo() {
+export type FFrLogoProps = {
+  size?: string;
+};
+
+export function FFrLogo(props: FFrLogoProps) {
   return (
-    <span className="font-mono text-2xl border border-primary/20 rounded-lg py-2 px-4 text-primary bg-primary/10 ">
+    <span
+      className={`font-mono ${props.size ?? 'text-2xl'} border border-primary/20 rounded-lg py-2 px-4 text-primary bg-primary/10`}>
       /ffremote
     </span>
   );
